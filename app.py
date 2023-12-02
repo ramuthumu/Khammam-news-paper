823ae973c80fa0a5e38c822f3c2197bfe489f43f
Ramu Thumu <ramuthumu@Ramus-MBP.attlocal.net>
2023-12-02 11:05:18 -0600

    mvp

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 
@@ -23,11 +23,41 @@ def fetch_and_parse_html(url):
 
     return articles
 
+def fetch_article_content(url):
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            return {"title": "Error", "content": "Content not available", "images": []}
+
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # Extract the title
+        title = soup.find("h1", class_="red").text.strip() if soup.find("h1", class_="red") else "No Title"
+
+        # Extract the main content
+        content_paragraphs = soup.select(".text-justify p")
+        content = ' '.join([para.get_text(strip=True) for para in content_paragraphs])
+
+        # Extract images
+        images = [img['src'] for img in soup.select(".text-justify img")]
+
+        return {"title": title, "content": content, "images": images}
+    except Exception as e:
+        return {"title": "Error", "content": "Content not available", "images": []}
+
+
+
 @app.route('/')
 def home():
     url = "https://www.eenadu.net/telangana/districts/khammam"
     articles = fetch_and_parse_html(url)
     return render_template('index.html', articles=articles)
+
+@app.route('/article')
+def article():
+    url = request.args.get('url')
+    content = fetch_article_content(url)
+    return render_template('article.html', content=content)
 
 if __name__ == '__main__':
     app.run(debug=True)
